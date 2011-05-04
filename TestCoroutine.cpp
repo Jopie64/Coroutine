b0753618e13b6a980afcd61b2a22a560a9e90ceb
Johan 't Hart <johanthart@gmail.com>
2011-05-05 01:33:00 +0200

    Made coro's actually work!

--- TestCoroutine.cpp
+++ TestCoroutine.cpp
@@ -9,7 +9,8 @@
 class CCoro
 {
 public:
-	CCoro(CCoro* P_MainCoroPtr):m_MainCoroPtr(P_MainCoroPtr), m_AddressPtr(NULL){}
+	CCoro(CCoro* P_MainCoroPtr):m_MainCoroPtr(P_MainCoroPtr), m_AddressPtr(NULL), m_bEnded(false), m_YieldingCoroPtr(NULL){}
+	~CCoro(){ if(m_AddressPtr != NULL)DeleteFiber(m_AddressPtr); }
 	static CCoro* Cur();
 
 	bool IsMain() const {return m_MainCoroPtr == NULL;}
@@ -20,6 +21,8 @@ public:
 			return Cur();
 		return Cur()->m_MainCoroPtr;
 	}
+
+	static CCoro* Initialize();
 
 	template<class TP_Cb>
 	class FcStartFunc;
@@ -35,16 +38,29 @@ public:
 		return W_CoroPtr;
 	}
 
-	void YieldTo(CCoro* P_YieldToPtr = NULL)
+	static void YieldTo(CCoro* P_YieldToPtr = NULL)
 	{
 		if(P_YieldToPtr == NULL)
 			P_YieldToPtr = Main();
+		P_YieldToPtr->m_YieldingCoroPtr = Cur();
 		SwitchToFiber(P_YieldToPtr->m_AddressPtr);
+		Cur()->OnResume();
 	}
+
+
 
 protected:
 	virtual void operator()() const =0;
 
+	void OnResume()
+	{
+		if(m_YieldingCoroPtr->m_bEnded)
+		{
+			//Coro yielding to me ended. Lets clean it up first.
+			delete m_YieldingCoroPtr;
+			m_YieldingCoroPtr = NULL;
+		}
+	}
 private:
 	CCoro(const CCoro&);
 	CCoro& operator=(const CCoro&);
@@ -52,17 +68,22 @@ private:
 	static void CALLBACK StartFunc(void* P_FuncPtr)
 	{
 		CCoro* W_CoroPtr = (CCoro*)P_FuncPtr;
+		W_CoroPtr->OnResume();
 		(*W_CoroPtr)();
-		delete W_CoroPtr;
+		W_CoroPtr->m_bEnded = true;
+		YieldTo();//Yield to main and kill me
 	}
 
 	CCoro*	m_MainCoroPtr;
+	CCoro*	m_YieldingCoroPtr;
 	void*	m_AddressPtr;
+	bool	m_bEnded;
 };
 
 template<class TP_Cb>
 class CCoro::FcStartFunc : public CCoro
 {
+friend CCoro;
 	FcStartFunc(CCoro* P_MainCoroPtr, const TP_Cb& P_Cb):CCoro(P_MainCoroPtr), m_Cb(P_Cb){}
 	void operator()() const { m_Cb(); }
 	TP_Cb m_Cb;
@@ -70,30 +91,63 @@ class CCoro::FcStartFunc : public CCoro
 
 class CCoro::FcMainStartFuncDummy : public CCoro
 {
-public:
+friend CCoro;
 	FcMainStartFuncDummy():CCoro(NULL){}
 	void operator()() const { throw std::logic_error("Calling the main coro's main function right? You cannot do that!"); }
 };
 
-CCoro* CCoro::Cur()
+CCoro* CCoro::Initialize()
 {
-	CCoro* W_ThisPtr = (CCoro*)GetFiberData();
-	if(W_ThisPtr != NULL)
-		return W_ThisPtr;
-
-	//There is no coro yet... Lets create the first of this thread.
-	W_ThisPtr = new FcMainStartFuncDummy;
+	//There should be no coro yet... Lets create the first one of this thread.
+	CCoro* W_ThisPtr = new FcMainStartFuncDummy;
 	W_ThisPtr->m_AddressPtr = ConvertThreadToFiber(W_ThisPtr);
 	return W_ThisPtr;
 }
 
+CCoro* CCoro::Cur()
+{
+	CCoro* W_ThisPtr = (CCoro*)GetFiberData();
+	if(W_ThisPtr == NULL)
+		throw std::runtime_error("Hey, I was expecting a Coro* fiber data structure, but I found NULL for this fiber!");
+		
+	return W_ThisPtr;
+}
 
 
 using namespace std;
+
+void Fuck()
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << "Fuck ";
+		CCoro::YieldTo();
+	}
+}
+
+void You()
+{
+	for(int i = 0; i < 10; ++i)
+	{
+		cout << "You!" << endl;
+		CCoro::YieldTo();
+	}
+}
+
 int _tmain(int argc, _TCHAR* argv[])
 {
-
+	CCoro::Initialize();
 	cout << "Hello world!" << endl;
+
+	CCoro* W_FuckPtr = CCoro::Create(std::tr1::bind(&Fuck));
+	CCoro* W_YouPtr = CCoro::Create(std::tr1::bind(&You));
+
+	for(int i = 0; i < 10; ++i)
+	{
+		CCoro::YieldTo(W_FuckPtr);
+		CCoro::YieldTo(W_YouPtr);
+	}
+
 
 	char c;
 	cin >> c;
